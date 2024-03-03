9e341f94cc5804e667b11be57e9926083c3afe13
xiaoyo8520 <129307103+xiaoyo8520@users.noreply.github.com>
2024-03-03 21:00:34 +0800

    Update np.py

--- np.py
+++ np.py
@@ -1,10 +1,40 @@
-from http.server import BaseHTTPRequestHandler
+import requests
+from flask import Flask, request,render_template
 
-class handler(BaseHTTPRequestHandler):
+app = Flask(__name__)
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    authorizeCode = request.args.get('code')
+    token = getNotifyToken(authorizeCode)
+    print(token)
+    lineNotifyMessage(token, "恭喜你連動")
+    return "aaaa"
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+@app.route('/ac/<name>/<msg>',methods=['POST','GET'])
+def hello_wor(name,msg):
+    token=f'{name}'
+    lineNotifyMessage(token, f'{msg}')
+    return "abcc"
+
+def getNotifyToken(AuthorizeCode): #運用網址'code'參數和相關資訊，生成oauth_token
+    body = {
+        "grant_type": "authorization_code",
+        "code": AuthorizeCode,
+        "redirect_uri": 'https://line-web-app.vercel.app/np.py',
+        "client_id": '9BhYhKzGtasty5jOlfsPGK',
+        "client_secret": 'j0KmCm6UHc14lyD0qQeQqf3NTER2K2p8vFpIsjlJRD5'
+    }
+    r=requests.post("https://notify-bot.line.me/oauth/token", data=body)
+    return r.json()["access_token"]
+
+def lineNotifyMessage(token, msg): #根據token，向notify發出post，發送msg
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    payload = {'message': msg}
+    r = requests.post("https://notify-api.line.me/api/notify", headers=headers, data=payload)
+    return r.status_code
+
+if __name__ == '__main__':
+    app.run()
